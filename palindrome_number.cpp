dc4c43d148f770c2a8cf53e2b58a0012e92beb21
dhruviladani <110341143+dhruviladani@users.noreply.github.com>
2024-03-06 09:57:44 +0530

    Create palindrome_number.cpp

---
+++ palindrome_number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string t = s;
+
+        reverse(s.begin(), s.end());
+        
+        return t==s;
+    }
+};
